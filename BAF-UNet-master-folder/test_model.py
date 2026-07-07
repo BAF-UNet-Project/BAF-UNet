26effd37d9f3b6047220a90cce2da7f1f8114019
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2026-07-07 20:15:46 +0000

    Update test_model.py

--- BAF-UNet-master-folder/test_model.py
+++ BAF-UNet-master-folder/test_model.py
@@ -46,11 +46,11 @@ model = BAFUNet(n_channels=3, n_classes=1).to(DEVICE)
 model.eval()   # Important: evaluation mode
 
 # Create dummy input (batch_size=8, 3 channels, 512x512)
-x = torch.randn(1, 3, 256, 256).to(DEVICE)
+x = torch.randn(2, 3, 512, 512).to(DEVICE)
 
 with torch.no_grad():        # No gradient calculation (faster + less memory)
     y = model(x)
 
 print("Input shape  :", x.shape)
 print("Output shape :", y.shape)
-print("Test passed!" if y.shape == (1, 1, 256, 256) else "❌ Shape mismatch!")
+print("Test passed!" if y.shape == (2, 1, 512, 512) else "❌ Shape mismatch!")
